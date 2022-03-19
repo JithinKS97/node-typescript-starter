da5f286f7a295e7c751d55f4ba12a24514809e29
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2022-03-20 00:06:54 +0530

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import RootRouter from "./routes/root";
-const cors = require("cors");
+import http from "http";
+import cors from "cors";
 
 const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 // Middlewares
 app.use(cors());
@@ -12,6 +19,18 @@ app.use(bodyParser.json());
 // Routes
 app.use("/", RootRouter);
 
-app.listen(5000, () => {
-  console.log("Server running");
+const PORT = 3001;
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
+
+io.on("connection", (socket: any) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("message", (msg: any) => {
+    io.emit("broadcast", msg);
+  });
 });
